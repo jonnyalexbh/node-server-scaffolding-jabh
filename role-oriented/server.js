bbf66a553d4cedcbf0f08b81c804ccda0994952e
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-10-27 10:21:22 -0500

    separate the book data from the main file

---
+++ role-oriented/server.js
@@ -0,0 +1,24 @@
+const { ApolloServer, gql } = require('apollo-server');
+const { infoBooks : serviceBooks } = require('./app/data');
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => serviceBooks,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
