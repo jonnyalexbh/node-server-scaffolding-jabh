8d49b8695aa53dfd8fcc99dd17baa530fe02d109
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-10-28 17:02:39 -0500

    init minimal with graphql-tools structure apollo server graphql

---
+++ minimal-graphql-tools/server.js
@@ -0,0 +1,34 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const serviceBooks = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => serviceBooks,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
