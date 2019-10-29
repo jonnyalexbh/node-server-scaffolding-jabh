0c2be3e878dff1858dc6d8203ba8c623285f42f8
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-10-29 09:06:52 -0500

    express graphql with build-schema

--- ways-to-write-graphql-schema/express-build-schema/server.js
+++ ways-to-write-graphql-schema/express-build-schema/server.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const { buildSchema } = require('graphql')
+const gqlMiddleware = require('express-graphql')
 
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res, next) => {
-  res.send({ greeting: "hello world" });
-})
+const schema = buildSchema(`
+  type Query {
+    hello: String,
+    greeting: String
+  }
+`)
+
+const resolvers = {
+  hello: () => {
+    return 'Hello World'
+  },
+  greeting: () => {
+    return 'Hello everyone'
+  }
+}
+
+app.use(
+  '/graphql',
+  gqlMiddleware({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true,
+  }),
+);
 
 const server = app.listen(port, function () {
   console.log(`listening http://localhost:${server.address().port}`);
