eb0c96e054d0f78ad302b478a8d487411cca25e2
jonnyalexbh <jonnyalex.bh@gmail.com>
2020-06-26 15:10:43 -0500

    docker compose nodejs postgresql

--- docker-compose-nodejs-pg/app.js
+++ docker-compose-nodejs-pg/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const models = require('./models/index');
 
 const app = express();
 const port = 3000;
 
 app.get('/', function (req, res) {
   res.send({ title: 'Docker compose Nodejs MySQL' })
+});
+
+app.get('/users', function (_, res) {
+  models.User.findAll().then((users) => {
+    console.log("All users:", JSON.stringify(users, null, 4));
+    res.send(users);
+  });
 });
 
 const server = app.listen(port, () => {
